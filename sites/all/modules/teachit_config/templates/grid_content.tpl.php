73ad07d863efbd3342602889ed0e15eba4b82e08
Home <tuananh3988@gmail.com>
2015-11-10 01:16:10 +0700

    home

---
+++ sites/all/modules/teachit_config/templates/grid_content.tpl.php
@@ -0,0 +1,29 @@
+<div class="wrap-grid-content">
+    <div class="grid-1">
+        <div class="wrap-content-1">
+            <div class="content">
+                <div class="overlay">
+                    <div>grid 1</div>
+                </div>
+                <div class="bg">
+                    <div class="bg-img" style="<?= empty(theme_get_setting("media-thumbnail-1")) ? '' : 'background-image: url(' . theme_get_setting("media-thumbnail-1") . ')'; ?>"></div>
+                </div>
+            </div>
+        </div>
+        <div class="wrap-content-2">
+            abc2
+        </div>
+        <div class="wrap-content-3">
+            abc3
+        </div>
+    </div>
+    <div class="grid-2">
+        123
+    </div>
+</div>
+
+<script>
+    jQuery(document).ready(function () {
+        
+    });
+</script>
